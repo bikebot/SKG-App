5ea755cb494f75dcca817c8c7387d044e5ed4758
Andreas C. Lemke <Andreas.Lemke@jambit.com>
2023-12-14 16:38:41 +0100

    Result of ChatGPT 4

--- app/src/main/java/de/skg_botnang/skg_app/LayoutTest.kt
+++ app/src/main/java/de/skg_botnang/skg_app/LayoutTest.kt
@@ -1,5 +1,7 @@
 package de.skg_botnang.skg_app
 
+import android.util.Log
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.animation.core.spring
@@ -23,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.platform.LocalDensity
@@ -33,21 +36,53 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.round
 import de.skg_botnang.skg_app.ui.theme.SKGAppTheme
 
+@Preview
 @Composable
-fun Num(num: Int, itemSize: Dp) {
+fun TestAnima() {
+    var offp: Boolean by remember { mutableStateOf(false) }
+    val col1 = MaterialTheme.colorScheme.primary
+    val col2 = MaterialTheme.colorScheme.secondary
+    var col: Color by remember { mutableStateOf(col1) }
+//    val color by animateColorAsState(
+//        targetValue = if(offp) MaterialTheme.colorScheme.primary
+//        else MaterialTheme.colorScheme.secondary
+//    )
+    Button(
+        onClick = {
+            offp = !offp
+            col = if (offp) col1 else col2
+        },
+        modifier = Modifier
+            .size(70.dp)
+            .clip(CircleShape)
+            .background(col)
+    ) {
+        Text(
+            "Col",
+            color = MaterialTheme.colorScheme.onPrimary,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun Circle(num: Int, itemSize: Dp) {
     var offp: Boolean by remember { mutableStateOf(false) }
     val offset by animateOffsetAsState(
         if(offp) Offset(0f, -40f) else Offset.Zero,
-        // tween(500, easing = FastOutSlowInEasing)
+        tween(500, easing = FastOutSlowInEasing)
         // spring()
     )
     Box(
         modifier = Modifier
-            .offset{ offset.round() }
+            .offset { offset.round() }
             .size(itemSize)
             .clip(CircleShape)
             .background(MaterialTheme.colorScheme.primary)
-            .clickable { offp = !offp },
+            .clickable {
+                offp = !offp
+                Log.d("SKG-App", "Num: $num, $offp")
+            },
         contentAlignment = Alignment.Center
     ) {
         Text(
@@ -62,7 +97,7 @@ fun Num(num: Int, itemSize: Dp) {
 @Preview
 @Composable
 fun LayoutTestScreen() {
-    val numItems = 10
+    val numItems = 7
     val itemSize = 70.dp
     val minSpacing = 20.dp
     val coordinates = remember { List(numItems) { mutableStateOf<LayoutCoordinates?>(null) } }
@@ -90,7 +125,7 @@ fun RowsWithEqualItems(numItems: Int, itemSize: Dp, minSpacing: Dp) {
     Layout(
         modifier = Modifier.fillMaxWidth(),
         content = {
-            repeat(numItems) { index -> Num(index, itemSize) }
+            repeat(numItems) { index -> Circle(index, itemSize) }
         }
     ) { measurables, constraints ->
         val constraints1 = constraints.copy(minWidth = 0, minHeight = 0)
@@ -127,6 +162,58 @@ fun RowsWithEqualItems(numItems: Int, itemSize: Dp, minSpacing: Dp) {
                         leftMargin = (width - itemsInLastRow * sizePlusSpacing + spacingPx) / 2
                     }
                 }
+            }
+        }
+    }
+}
+
+
+// Solution of ChatGPT 4
+
+// Hi ChatGPT, let's develop a JetPack Compose function that lays out a number of circles in rows.
+// Use the minimum necessary number of rows. Distribute the circles as equally as possible into the
+// rows. The circles shall be spaced by a specific distance and they shall be centered in their
+// row. The circles are to produced with this composable function: Circle...
+
+// Rewrite CirclesLayout using the Layout composable instead of Column.
+@Composable
+fun CirclesLayout(totalCircles: Int, itemSize: Dp, spacing: Dp) {
+    Layout(
+        content = {
+            for (i in 0 until totalCircles) {
+                Circle(num = i, itemSize = itemSize)
+            }
+        }
+    ) { measurables, constraints ->
+        // Calculate the maximum number of circles per row based on the screen width
+        val maxCirclesPerRow = ((constraints.maxWidth - spacing.roundToPx()) / (itemSize.toPx() + spacing.toPx())).toInt()
+
+        // Define the size of each item
+        val itemConstraints = constraints.copy(minWidth = itemSize.roundToPx(), minHeight = itemSize.roundToPx())
+
+        // Measure each child
+        val placeables = measurables.map { measurable ->
+            measurable.measure(itemConstraints)
+        }
+
+        // Calculate the size of the layout
+        val numRows = ceil(totalCircles.toFloat() / maxCirclesPerRow).toInt()
+        val layoutWidth = constraints.maxWidth
+        val layoutHeight = (itemSize.toPx() * numRows + spacing.toPx() * (numRows - 1)).toInt()
+
+        // Define the layout
+        layout(layoutWidth, layoutHeight) {
+            var yPosition = 0
+
+            placeables.chunked(maxCirclesPerRow).forEach { row ->
+                var xPosition = (layoutWidth - (row.size * itemSize.toPx() + (row.size - 1) * spacing.toPx())) / 2f
+
+                row.forEach { placeable ->
+                    placeable.placeRelative(x = xPosition.roundToInt(), y = yPosition)
+                    xPosition += itemSize.toPx() + spacing.toPx()
+                }
+
+                yPosition += itemSize.roundToPx() + spacing.roundToPx()
             }
         }
     }
